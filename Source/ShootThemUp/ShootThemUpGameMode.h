326ffa7f3d66f892614a40044b8a9412575ee76f
acdc4567 <acdc4567@gmail.com>
2022-04-01 13:22:12 +0530

    ShootThemUpGame
    NewVersion

---
+++ Source/ShootThemUp/ShootThemUpGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootThemUpGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShootThemUpGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShootThemUpGameMode();
+};
+
+
+
